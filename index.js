85bf661d6a2a3cdb2ff1a609f059cb02fd294e16
hckim96 <hyeoncheolkim96@gmail.com>
2020-07-15 11:31:35 +0900

    port

--- index.js
+++ index.js
@@ -4,16 +4,13 @@ import resolvers from './graphql/resolvers';
 const server = new GraphQLServer({
     typeDefs: './graphql/schema.graphql',
     resolvers: resolvers,
-    opts: {
-        port: process.env.PORT || '4000',
-        endpoint: 'graphql',
-    },
 });
 server.start(
     {
-        port: 4000,
+        port: process.env.PORT || 4000,
         endpoint: '/graphql',
         playground: '/playground',
+        subscriptions: '/subscriptions',
     },
     ({ port }) => {
         console.log('graphql server started', port);
